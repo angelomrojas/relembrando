c9fbcde37e9a4b7b1ce997245b679d07514dcc32
Angelo Rojas <angelo.maeda@hotmail.com>
2024-04-24 07:48:09 -0300

    branch

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let listaDeNumerosSorteados = [];
 let numeroLimite = 7000;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
+let branch = "teste1"
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
